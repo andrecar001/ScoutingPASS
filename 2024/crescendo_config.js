0f4cdff7380d073a445a6357c7cf5c7fc9d6040f
Carlos Andrews <crdandrews@gmail.com>
2024-02-18 04:09:04 -0600

    add createArray, to create array based off text file

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -1,6 +1,6 @@
 var config_data = `
 {
-  "dataFormat": "tsv",
+  "dataFormat": "kvs",
   "title": "Scouting PASS 2024",
   "page_title": "Crescendo, Team 2508",
   "checkboxAs": "10",
--- resources/js/data_rep.js
+++ resources/js/data_rep.js
@@ -1,7 +1,197 @@
-var data,temp
-var xlsx = require("xlsx")
-var fs = require("fs")
-var excelFile = "../../Excel/TestSheet.xlsx"
-var wb = xlsx.readFile(excelFile)
-var sheetNames = wb.sheetNames
-console.log(sheetNames)
+//data_rep.js
+//
+// The data representation application
+// Written by Team 2508 - Armada
+// Influenced by Team 2451 - PWNAGE
+
+document.addEventListener("touchstart",startTouch,false)
+document.addEventListener("touchend",moveTouch,false)
+
+// Swipe Up / Down / Left / Right
+var initialX = null
+var xThreshold = 0.3
+var slide = 0
+var checkboxAs = 'YN'
+
+//Required Format for data
+var requiredScoutingFields = ["s","e","m","r","t","as","dir","al","aas","ass","tss","tta","tpu","dt","fs","nit","dr","sr","die","tie","dn","co"]
+var requiredPitFields = ["t","crn","wid","wei","drv","odt","sr","mot","nob","fpu","ss","sa","def","driv","clmb","aut","dts","co"]
+
+
+
+async function create2dFileArray(fileName) {
+    let fileText
+    try{
+        var res = await fetch(fileName)
+        var text = await res.text()
+        fileText = text
+    } catch (e) {
+        console.error('Error fetching data', e)
+    }
+    
+    
+    lines = fileText.trim().split('\n')
+    var splitArray = lines.map(line => {
+        let splitLine = line.split('\t')
+        let lastElement = splitLine[splitLine.length -1]
+        let lastElementIdx = splitLine.length-1
+        //let returnLine = splitLine[lastElementIdx].slice(0,-1)
+        if (lastElement.charAt(lastElement.length-1) === '\r'){
+            splitLine[lastElementIdx] = lastElement.slice(0,-1)
+            return splitLine
+        } else {
+            return splitLine
+        }
+        return returnLine
+    })
+   
+    return splitArray
+}
+
+async function createTable(fileName){
+
+    array = await create2dFileArray(fileName)
+    console.log(array)
+    var container = document.createElement('div')
+    container.setAttribute('id', 'textData')
+    document.getElementById('allData_table').appendChild(container)
+    document.getElementById('textData').innerText = array
+}
+
+
+function addTable(table, idx, name, data) {
+    var container = document.createElement('div')
+    container.style.border = '1px solid black'
+    container.style.padding = '10px'
+    //document.getElementById('allData_table').appendChild(container)
+    var row = table.insertTow(idx)
+    var cell1 = row.insertCell(0)
+    cell1.classList.add('title')
+    var cell2 = row.insertCell(1)
+    cell1.innerHTML = name + '&nbsp;'
+    cell2.classList.add('field')
+
+    data.forEach(sheetRowData => {
+        var sheetRow = document.createElement('div')
+        sheetRow.style.display = 'flex'
+
+        sheetRowData.forEach(cellData => {
+            var cell = document.createElement('div')
+            cell.textContent = cellData
+            cell.style.border = '1px solid black'
+            cell.style.minWidth = '50px'
+            cell.style.padding = '5px'
+            sheetRow.appendChild(cell)
+        })
+
+        container.appendChild(sheetRow)
+    })
+    cell2.appendChild(container)
+    return idx + 1
+}
+
+function addTeamNumber(table, idx, name, data) {
+    var row = table.insertRow(idx)
+    var cell1 = row.insertCell(0)
+    cell1.classList.add('title')
+    var cell2 = row.insertCell(1)
+    cell1.innerHTML = name + '&nbsp;'
+    cell2.classList.add('field')
+
+    var inp = document.createElement('input')
+    inp.setAttribute('id','input_team')
+    inp.setAttribute('type', 'number')
+    inp.setAttribute('size', 5)
+    inp.setAttribute('min', 999)
+    inp.setAttribute('max', 9999)
+    inp.setAttribute('defaultValue', 2508)
+    inp.setAttribute('required',"")
+    cell2.appendChild(inp)
+    return idx +1
+}
+
+function addElement(table,idx,data) {
+    var type = null
+    var name = 'Default Name'
+    if (data.hasOwnProperty('name')) {
+        name = data.name
+      }
+    if (data.hasOwnProperty('type')) {
+    type = data.type
+    } else {
+    console.log("No type specified");
+    console.log("Data: ")
+    console.log(data);
+    err = { code: "err", defaultValue: "No type specified: " + data };
+    idx = addText(table, idx, name, err);
+    return
+    }
+    if (data.type == 'text'){idx = addTeamText(table, idx, name, data)}
+    else if (data.type == 'table'){idx = addTable(table, idx, name, data)}
+    else {console.log(`Unrecongnized type: ${data.type}`)}
+    return idx
+}
+
+function startTouch(e) {
+    initialX = e.touches[0].screenX;
+}
+
+function moveTouch(e) {
+    if (initialX === null) {
+      return;
+    }
+  
+    var currentX = e.changedTouches[0].screenX;
+    var diffX = initialX - currentX;
+  
+    // sliding horizontally
+    if (diffX / screen.width > xThreshold) {
+      // swiped left
+      swipePage(1);
+    } else if (diffX / screen.width < -xThreshold) {
+      // swiped right
+      swipePage(-1);
+    }
+    initialX = null;
+}
+function getIdBase(name) {
+    return name.slice(name.indexOf("_"), name.length)
+}
+// Sample data for the spreadsheet
+
+
+function test() {
+    const data = [
+        ['A1', 'B1', 'C1'],
+        ['A2', 'B2', 'C2'],
+        ['A3', 'B3', 'C3']
+    ];
+    // Create a container div to hold the spreadsheet
+    const container = document.createElement('div');
+    container.style.border = '1px solid black';
+    container.style.backgroundColor = 'grey';
+    container.style.padding = '10px';
+    document.getElementById('allData_table').appendChild(container);
+
+    // Loop through the data and create rows and cells
+    data.forEach(rowData => {
+        const row = document.createElement('div');
+        row.style.display = 'flex';
+
+        rowData.forEach(cellData => {
+            const cell = document.createElement('div');
+            cell.textContent = cellData;
+            cell.style.border = '1px solid black';
+            cell.style.minWidth = '50px';
+            cell.style.padding = '5px';
+            row.appendChild(cell);
+        });
+
+        container.appendChild(row);
+    });
+}
+
+window.onload = function() {
+    createTable('../../data/scoutingData.txt')
+    test()
+}
