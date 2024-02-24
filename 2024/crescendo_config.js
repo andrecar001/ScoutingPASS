f7248d97e35cb0a2d6e9c431f328efcbdbeb860c
andrecar001 <crdandrews@gmail.com>
2024-02-24 09:14:58 -0600

    update spreadsheet and config

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "Grand Forks",
+      "defaultValue": "Week 0",
       "required": "true"
     },
     { "name": "Match Level",
